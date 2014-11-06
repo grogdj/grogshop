1400414cd12d2f930413a69c863e02448bf791f1
Ezequiel <ezequielsalatino@MacBook-Air-de-Ezequiel.local>
2014-11-05 22:53:25 -0300

    Forms

--- grogshop-server/src/main/webapp/js/bids.js
+++ grogshop-server/src/main/webapp/js/bids.js
@@ -12,33 +12,7 @@
 
 				//$http.get("http://grogshop-restprovider.rhcloud.com/grogshop-server/rest/listings/all").success(function(data){
 
-				 	shop.bids = [
-                             {
-							    "userId" : "example",
-							    "amount" : 300,
-							    "tags" : [
-							      {
-							        "name" : "#tag1"
-							      },
-							      {
-							        "name" : "#tag2"
-							      }
-							    ]
-							  },
-							  {
-							    "userId" : "Ezequiel",
-							    "amount" : 700,
-							    "tags" : [
-							      {
-							        "name" : "#tag1"
-							      },
-							      {
-							        "name" : "#tag2"
-							      }
-							    ]
-							  }      
-                                   
-                    ];
+				 	shop.bids = bidsdata;
                                 
 				// });
 
@@ -48,6 +22,56 @@
 			controllerAs: 'bidsCtrl'
 		}
 	}]);
+
+	app.directive("bidsForm", ['$http',function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'bids-form.html',
+			controller: function($http, $scope){
+
+				var shop = this
+				shop.livebid = {};
+
+				this.addBid = function(formCtrl) {
+					alert("estas creando un nuevo bid - " + formCtrl.livebid.userId)
+					// ACA VA LA LOGICA QUE GUARDE LOS DATOS CON POST
+					shop.livebid= {};
+					 $scope.bidform.$setPristine();
+
+				}
+			},
+			controllerAs: 'bidsFromCtrl'
+		}
+	}]);
+
+
+	var bidsdata = [
+             {
+			    "userId" : "example",
+			    "amount" : 300,
+			    "tags" : [
+			      {
+			        "name" : "#tag1"
+			      },
+			      {
+			        "name" : "#tag2"
+			      }
+			    ]
+			  },
+			  {
+			    "userId" : "Ezequiel",
+			    "amount" : 700,
+			    "tags" : [
+			      {
+			        "name" : "#tag1"
+			      },
+			      {
+			        "name" : "#tag2"
+			      }
+			    ]
+			  }      
+                   
+    ];
 
 })();
 
