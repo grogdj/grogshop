267a9bbcb9925614dfd1016b47ee82c927fd9979
grogdj <grogdj@gmail.com>
2014-11-10 18:18:51 +0000

    - adding push to listings and bids

--- grogshop-server/src/main/webapp/js/bids.js
+++ grogshop-server/src/main/webapp/js/bids.js
@@ -1,54 +1,58 @@
-(function(){
-	var app = angular.module('shopbids',[]);
+(function () {
+    var app = angular.module('shopbids', []);
 
-	app.directive("bidsItem", ['$http', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'bids-item.html',
-			controller: function ($http){
-				
-				var shop = this;
-				shop.bids = [];
+    app.directive("bidsItem", ['$http', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'bids-item.html',
+                controller: function ($http, $rootScope) {
 
-				$http.get("http://localhost:8080/grogshop-server/rest/bids/all").success(function(data){
+                    var shop = this;
+                    shop.bids = [];
 
-				 	shop.bids = data;
-                                
-                                
-				 });
+                    $http.get("http://localhost:8080/grogshop-server/rest/bids/all").success(function (data) {
+
+                        shop.bids = data;
 
 
-				
-			},
-			controllerAs: 'bidsCtrl'
-		}
-	}]);
-
-	app.directive("bidsForm", ['$http',function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'bids-form.html',
-			controller: function($http, $scope){
-
-				var shop = this
-				shop.livebid = {};
-
-				this.addBid = function() {
-					
-					//
-					$http.post('http://localhost:8080/grogshop-server/rest/bids/new',{userId:shop.livebid.userId, amount:shop.livebid.amount, tags:[shop.livebid.tags]});
-					//
-					shop.livebid= {};
-					 $scope.bidform.$setPristine();
-
-				}
-			},
-			controllerAs: 'bidsFromCtrl'
-		}
-	}]);
+                    });
+                    $rootScope.$on('newBid', function (event, data) {
+                        console.log(data);
+                        shop.bids.push(data);
+                    });
 
 
-	
+                },
+                controllerAs: 'bidsCtrl'
+            }
+        }]);
+
+    app.directive("bidsForm", ['$http', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'bids-form.html',
+                controller: function ($http, $scope) {
+
+                    var shop = this
+                    shop.livebid = {};
+
+                    this.addBid = function () {
+                        var bid =  {userId: shop.livebid.userId, amount: shop.livebid.amount, tags: [shop.livebid.tags]}
+                        //
+                        $http.post('http://localhost:8080/grogshop-server/rest/bids/new',bid);
+                        //
+                        $scope.$broadcast('newBid', bid);
+                        shop.livebid = {};
+                        $scope.bidform.$setPristine();
+
+                    }
+                },
+                controllerAs: 'bidsFromCtrl'
+            }
+        }]);
+
+
+
 
 })();
 
--- grogshop-server/src/main/webapp/js/listings.js
+++ grogshop-server/src/main/webapp/js/listings.js
@@ -1,49 +1,54 @@
-(function(){
-	var app = angular.module('shoplistings',[]);
+(function () {
+    var app = angular.module('shoplistings', []);
+    
+    app.directive("listingItem", ['$http', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'listing-item.html',
+                controller: function ($http, $rootScope) {
 
-	app.directive("listingItem", ['$http', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'listing-item.html',
-			controller: function ($http){
-				
-				var shop = this;
-				shop.products = [];
+                    var shop = this;
+                    shop.products = [];
 
-				$http.get("http://localhost:8080/grogshop-server/rest/listings/all").success(function(data){
+                    $http.get("http://localhost:8080/grogshop-server/rest/listings/all").success(function (data) {
 
-				 	shop.products = data;
-                             
-                                
-				 });
+                        shop.products = data;
 
 
-				
-			},
-			controllerAs: 'listingsCtrl'
-		}
-	}]);
+                    });
+                    
+                    $rootScope.$on('newListing', function (event, data) {
+                        console.log(data);
+                        shop.products.push(data);
+                    });
 
-  app.directive("listingForm", ['$http',function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'listings-form.html',
-      controller: function($http, $scope){
 
-        var shop = this
-        shop.livelisting = {};
 
-        this.addListing = function() {
-        
-          $http.post('http://localhost:8080/grogshop-server/rest/listings/new',{userId:shop.livelisting.userId, price:shop.livelisting.price, tags:[shop.livelisting.tags]});
-          shop.livelisting= {};
-          $scope.listingform.$setPristine();
+                },
+                controllerAs: 'listingsCtrl'
+            }
+        }]);
 
-        }
-      },
-      controllerAs: 'listingFromCtrl'
-    }
-  }]);
+    app.directive("listingForm", ['$http',   function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'listings-form.html',
+                controller: function ($http, $scope) {
+
+                    var shop = this
+                    shop.livelisting = {};
+                    this.addListing = function () {
+                        var listing = {userId: shop.livelisting.userId, price: shop.livelisting.price, tags: [shop.livelisting.tags]};
+                        $http.post('http://localhost:8080/grogshop-server/rest/listings/new', listing);
+                        $scope.$broadcast('newListing', listing);
+                        shop.livelisting = {};
+                        $scope.listingform.$setPristine();
+
+                    }
+                },
+                controllerAs: 'listingFromCtrl'
+            }
+        }]);
 
 
 })();
