c0e2306f2a560f27db77fa4ace5fc863aff0ad97
Ezequiel <ezequiel.salatino@gmail.com>
2015-01-05 18:48:50 +0000

    first login layout

--- grogshop-server/src/main/webapp/controllers/firstLoginController.js
+++ grogshop-server/src/main/webapp/controllers/firstLoginController.js
@@ -39,4 +39,56 @@ app.controller('firstLoginController', function ($rootScope, $http, $scope) {
     };
     
     $scope.newProfile($scope.user_id, $scope.email, $scope.auth_token);
+
+
+    //GRID
+    $scope.imagePath = "resources/img/maintag-images/"
+    $scope.mainTags = [
+		{name:"music", image:"music.jpg"},
+		{name:"art", image:"art.jpg"},
+		{name:"science", image:"science.jpg"},
+		{name:"sports", image:"sports.jpg"},
+		{name:"cars", image:"cars.jpg"},
+		{name:"cooking", image:"cooking.jpg"},
+		{name:"design", image:"design.jpg"},
+		{name:"health", image:"health.jpg"},
+		{name:"music", image:"music.jpg"},
+		{name:"art", image:"art.jpg"},
+		{name:"science", image:"science.jpg"},
+		{name:"sports", image:"sports.jpg"},
+		{name:"cars", image:"cars.jpg"},
+		{name:"cooking", image:"cooking.jpg"},
+		{name:"design", image:"design.jpg"},
+		{name:"health", image:"health.jpg"}
+		
+	];
+
+	$scope.numberOfItem = Math.floor($( window ).width() / 300);
+	console.log("items per row " + $scope.numberOfItem);
+	$scope.pageClass="firstLoginScreen";
+	$scope.elementWidth = $( window ).width() / $scope.numberOfItem ;
+	$scope.elementHeight = ($( window ).height()-50) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem)) ;
+	
+	$( window ).resize(function() {
+	  $scope.numberOfItem = Math.floor($( window ).width() / 300);
+	  $scope.elementWidth = $( window ).width() / $scope.numberOfItem ;
+	  $scope.elementHeight = ($( window ).height()-50) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem)) ;
+	  $scope.$digest();
+	  console.log("elementWidth " + $scope.elementWidth)
+	});
+
+	//CLICK CHECK
+	$scope.selectedTagsName = [];
+	$scope.selectedTags = [];
+	$scope.toogleCheck = function(valueName){
+		if($scope.selectedTagsName.indexOf(valueName) > -1){
+			$scope.selectedTagsName.splice($scope.selectedTagsName.indexOf(valueName),1);
+		}else {
+			$scope.selectedTagsName.push(valueName);
+		};
+		console.log($scope.selectedTagsName);
+	}
+
+
+
 });
