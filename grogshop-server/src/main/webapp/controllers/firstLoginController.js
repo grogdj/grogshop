f177cecf545722fd719cf760c7e7a6934bb5edb6
Ezequiel <ezequiel.salatino@gmail.com>
2015-01-06 11:28:55 +0000

    First Login styles

--- grogshop-server/src/main/webapp/controllers/firstLoginController.js
+++ grogshop-server/src/main/webapp/controllers/firstLoginController.js
@@ -71,7 +71,44 @@ app.controller('firstLoginController', function ($rootScope, $http, $scope, $coo
             $rootScope.$broadcast("quickNotification", "Tags loaded!");
 
             $scope.mainTags = data;
+            $scope.initImages();
 
+
+        }).error(function (data) {
+            console.log("Error: " + data);
+            $rootScope.$broadcast("quickNotification", "Something went wrong!" + data);
+        });
+
+    };
+
+     $scope.loadUserTags = function (user_id, email, auth_token) {
+        console.log("creating profile for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
+
+        $http({
+            method: 'GET',
+            url: 'rest/users/'+user_id+"/interests",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded', service_key: 'webkey:' + email, auth_token: auth_token},
+            transformRequest: function (obj) {
+                var str = [];
+                for (var key in obj) {
+                    if (obj[key] instanceof Array) {
+                        for (var idx in obj[key]) {
+                            var subObj = obj[key][idx];
+                            for (var subKey in subObj) {
+                                str.push(encodeURIComponent(key) + "[" + idx + "][" + encodeURIComponent(subKey) + "]=" + encodeURIComponent(subObj[subKey]));
+                            }
+                        }
+                    }
+                    else {
+                        str.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]));
+                    }
+                }
+                return str.join("&");
+            },
+            data: {user_id: user_id}
+        }).success(function (data) {
+            $rootScope.$broadcast("quickNotification", "Users Tags loaded!");
+            $scope.selectedTagsName = data;
 
         }).error(function (data) {
             console.log("Error: " + data);
@@ -122,20 +159,23 @@ app.controller('firstLoginController', function ($rootScope, $http, $scope, $coo
         $scope.newProfile($scope.user_id, $scope.email, $scope.auth_token);
     }
     $scope.loadMainTags($scope.user_id, $scope.email, $scope.auth_token);
+    $scope.loadUserTags($scope.user_id, $scope.email, $scope.auth_token);
 
+    $scope.initImages = function(){
+        $scope.numberOfItem = Math.floor($(window).width() / 300);
+        $scope.pageClass = "firstLoginScreen";
+        $scope.elementWidth = (($(window).width()-4) / $scope.numberOfItem)-4;
+        $scope.elementHeight = (($(window).height() - 54) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem)))-4;
+        
+    }
 
-    $scope.numberOfItem = Math.floor($(window).width() / 300);
-    console.log("items per row " + $scope.numberOfItem);
-    $scope.pageClass = "firstLoginScreen";
-    $scope.elementWidth = $(window).width() / $scope.numberOfItem;
-    $scope.elementHeight = ($(window).height() - 50) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem));
 
     $(window).resize(function () {
         $scope.numberOfItem = Math.floor($(window).width() / 300);
-        $scope.elementWidth = $(window).width() / $scope.numberOfItem;
-        $scope.elementHeight = ($(window).height() - 50) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem));
+        $scope.elementWidth = (($(window).width()-4) / $scope.numberOfItem)-4;
+        $scope.elementHeight = (($(window).height() - 54) / Math.ceil(($scope.mainTags.length / $scope.numberOfItem)))-4;
         $scope.$digest();
-        console.log("elementWidth " + $scope.elementWidth)
+        
     });
 
     //CLICK CHECK
