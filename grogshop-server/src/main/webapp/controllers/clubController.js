5875c78e4062887e7700cca2774fe027039ca1dd
esala <ezequiel.salatino@gmail.com>
2015-01-12 18:03:59 +0000

    Add Post Form

--- grogshop-server/src/main/webapp/controllers/clubController.js
+++ grogshop-server/src/main/webapp/controllers/clubController.js
@@ -1,31 +1,23 @@
 app.controller('clubController', function ($scope, $routeParams, $http, $rootScope, $location) {
-
+    
     $scope.imagePath = "static/img/public-images/"
 
     $scope.club_id = $routeParams.club_id;
     $scope.club = {};
     $scope.url = $location.path();
     $scope.isPreview = $scope.url.indexOf('preview');
-    $scope.products = [{'name': 'My Product', 'price': '124.12351', 'desc': 'My product is unique'}, {'name': 'Other Product', 'price': '0.5', 'desc': 'Description'}, {'name': 'Expensive Product', 'price': '7000.5', 'desc': 'Other product description'}, {'name': 'My Product', 'price': '124.12351', 'desc': 'My product is uniqu adsask dlja lsdjaskld jaslkd asd as dajs ldasj dklasj dlkas jdaskl djaslk djasld jaslk djasl djaslk djasl das dljas kldjsa lkdja lkdja sldje'}, {'name': 'Other Product', 'price': '0.5', 'desc': 'Description'}, {'name': 'Expensive Product', 'price': '7000.5', 'desc': 'Other product description'}, {'name': 'My Product', 'price': '124.12351', 'desc': 'My product is unique'}, {'name': 'Other Product', 'price': '0.5', 'desc': 'Description'}, {'name': 'Expensive Product', 'price': '7000.5', 'desc': 'Other product description'}];
-
-    $scope.productWidth = $(window).width() / 5;
-
-
-
-    if ($scope.isPreview > 0) {
-        $scope.pageClass = "clubPreview";
-    } else {
-        $scope.pageClass = "clubDetail";
-    }
-    console.log("HAS MEMB = " + $scope.hasMembership($scope.club_id));
+    $scope.products = [{'name':'My Product', 'price':'124.12351', 'desc':'My product is unique'}, {'name':'Other Product', 'price':'0.5', 'desc':'Description'}, {'name':'Expensive Product', 'price':'7000.5', 'desc':'Other product description'},{'name':'My Product', 'price':'124.12351', 'desc':'My product is uniqu adsask dlja lsdjaskld jaslkd asd as dajs ldasj dklasj dlkas jdaskl djaslk djasld jaslk djasl djaslk djasl das dljas kldjsa lkdja lkdja sldje'}, {'name':'Other Product', 'price':'0.5', 'desc':'Description'}, {'name':'Expensive Product', 'price':'7000.5', 'desc':'Other product description'},{'name':'My Product', 'price':'124.12351', 'desc':'My product is unique'}, {'name':'Other Product', 'price':'0.5', 'desc':'Description'}, {'name':'Expensive Product', 'price':'7000.5', 'desc':'Other product description'}];
+    
+    $scope.productWidth = $( window ).width()/5;
+    
     console.log("Es preview? " + $scope.isPreview);
-    if ($scope.hasMembership($scope.club_id) && $scope.isPreview > 0) {
-        $rootScope.$broadcast('goTo', "/club/" + $scope.club_id);
-    } else if (!$scope.hasMembership($scope.club_id) && $scope.isPreview < 0) {
-        $rootScope.$broadcast('goTo', "/club/preview/" + $scope.club_id);
+    
+    if($scope.isPreview > 0 ){
+       $scope.pageClass = "clubPreview";
+    }else {
+       $scope.pageClass = "clubDetail"; 
     }
-
-
+    
     $scope.loadClub = function (user_id, email, auth_token) {
         console.log("loading clubs for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
 
@@ -37,25 +29,6 @@ app.controller('clubController', function ($scope, $routeParams, $http, $rootSco
             data: {}
         }).success(function (data) {
             $rootScope.$broadcast("quickNotification", "Club loaded!");
-            $scope.club = data;
-        }).error(function (data) {
-            console.log("Error: " + data);
-            $rootScope.$broadcast("quickNotification", "Something went wrong!" + data);
-        });
-
-    };
-
-    $scope.loadPublicClub = function (user_id, email, auth_token) {
-        console.log("loading public club (" + $scope.club_id + ") for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
-
-        $http({
-            method: 'GET',
-            url: 'rest/public/clubs/' + $scope.club_id,
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            transformRequest: transformRequestToForm,
-            data: {}
-        }).success(function (data) {
-            $rootScope.$broadcast("quickNotification", "Public Club loaded!");
             $scope.club = data;
         }).error(function (data) {
             console.log("Error: " + data);
@@ -77,17 +50,16 @@ app.controller('clubController', function ($scope, $routeParams, $http, $rootSco
         }).success(function (data) {
             $rootScope.$broadcast("quickNotification", "Club Joined!");
             $scope.memberships.push(parseInt(club_id));
-            $rootScope.$broadcast('goTo', "/club/" + $scope.club_id);
-            console.log("after joining the club: " + $rootScope.memberships);
+            console.log("after joining the club: "+$rootScope.memberships);
         }).error(function (data) {
             console.log("Error: " + data);
             $rootScope.$broadcast("quickNotification", "Something went wrong!" + data);
         });
 
     };
-
+    
     $scope.cancelMembership = function (club_id, user_id, email, auth_token) {
-        console.log("canceling membership (" + club_id + ") for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
+        console.log("canceling membership ("+club_id+") for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
 
         $http({
             method: 'POST',
@@ -98,8 +70,7 @@ app.controller('clubController', function ($scope, $routeParams, $http, $rootSco
         }).success(function (data) {
             $rootScope.$broadcast("quickNotification", "Club Membership Cancelled!");
             $scope.memberships.splice($scope.memberships.indexOf(parseInt(club_id)), 1);
-            console.log("after canceling the club: " + $scope.memberships);
-            $rootScope.$broadcast('goTo', "/club/preview/" + $scope.club_id);
+            console.log("after canceling the club: "+$scope.memberships);
         }).error(function (data) {
             console.log("Error: " + data);
             $rootScope.$broadcast("quickNotification", "Something went wrong!" + data);
@@ -107,13 +78,19 @@ app.controller('clubController', function ($scope, $routeParams, $http, $rootSco
 
     };
 
-    if ($scope.auth_token && $scope.auth_token !== "") {
-        console.log("loading private clubs because: " + $scope.auth_token);
-        $scope.loadClub($scope.user_id, $scope.email, $scope.auth_token);
-    } else {
-        console.log("loading public clubs because: " + $scope.auth_token);
-        $scope.loadPublicClub($scope.user_id, $scope.email, $scope.auth_token);
-    }
-
+    $scope.loadClub($scope.user_id, $scope.email, $scope.auth_token);
+    
+    
+    $scope.smallerThan = function(a,b) {
+      return a <= b;
+    };
+    $scope.greaterThan = function(a,b) {
+      return a >= b;
+    };
+    
+    
+    $scope.addPost = function(isValid){
+        //ADD POST
+    };
 
 });
