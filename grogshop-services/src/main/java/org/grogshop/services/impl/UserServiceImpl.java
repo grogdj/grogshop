4e880ad7eb4a89dc71ad5ec85cd8042f34975676
grogdj <grogdj@gmail.com>
2015-01-14 16:47:45 +0000

    - adding two default users

--- grogshop-services/src/main/java/org/grogshop/services/impl/UserServiceImpl.java
+++ grogshop-services/src/main/java/org/grogshop/services/impl/UserServiceImpl.java
@@ -10,10 +10,17 @@ import com.grogdj.grogshop.core.model.User;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.inject.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import org.grogshop.services.api.UserService;
 import org.grogshop.services.exceptions.ServiceException;
 import org.grogshop.services.util.GrogUtil;
@@ -28,33 +35,39 @@ public class UserServiceImpl implements UserService {
     @PersistenceContext(unitName = "primary")
     private EntityManager em;
 
-    
-    private final static Logger log =  Logger.getLogger( UserServiceImpl.class.getName() );
-    
+    @Resource
+    UserTransaction ut;
+
+    private final static Logger log = Logger.getLogger(UserServiceImpl.class.getName());
+
     public UserServiceImpl() {
     }
-    
+
     @PostConstruct
     private void init() {
-//        try {
-//            newUser(new User("salaboy@gmail.com", "asdasd"));
-//            newUser(new User("eze@asd.asd", "123123"));
-//        } catch (ServiceException ex) {
-//            Logger.getLogger(UserServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        
+
+        try {
+            ut.begin();
+            newUser(new User("salaboy@gmail.com", "asdasd"));
+            newUser(new User("eze@asd.asd", "123123"));
+            ut.commit();
+
+        } catch (Exception ex) {
+            Logger.getLogger(UserServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     @Override
     public void newUser(User user) throws ServiceException {
         if (getByEmail(user.getEmail()) != null) {
-            throw new ServiceException( "User already registered with email: " + user.getEmail(), false);
+            throw new ServiceException("User already registered with email: " + user.getEmail(), false);
         }
         user.setPassword(GrogUtil.hash(user.getPassword()));
         em.persist(user);
         String key = generateWebKey(user.getEmail());
         log.log(Level.INFO, "User {0} registered. Service Key: {1}", new Object[]{user.getEmail(), key});
-        
+
     }
 
     @Override
