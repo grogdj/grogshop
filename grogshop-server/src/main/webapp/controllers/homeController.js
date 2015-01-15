e3458595cd6c4df8ef2d34c8c1c07729d16c060e
esala <ezequiel.salatino@gmail.com>
2015-01-15 17:32:37 +0000

    Filter interest in home

--- grogshop-server/src/main/webapp/controllers/homeController.js
+++ grogshop-server/src/main/webapp/controllers/homeController.js
@@ -3,6 +3,7 @@ app.controller('homeController', function ($scope, $http, $rootScope) {
     $scope.imagePath = "static/img/public-images/"
     
     $scope.interests = [];
+    $scope.userInterests = [];
     
      $scope.loadUserInterests = function (user_id, email, auth_token) {
         console.log("loading interests for user " + user_id + " with email: " + email + " and auth_token: " + auth_token);
@@ -16,6 +17,10 @@ app.controller('homeController', function ($scope, $http, $rootScope) {
         }).success(function (data) {
             //$rootScope.$broadcast("quickNotification", "User Interest loaded!");
             $scope.interests = data;
+            for (x in $scope.interests) {
+                $scope.userInterests.push($scope.interests[x].name);
+            }
+            console.log($scope.userInterests);
         }).error(function (data) {
             console.log("Error: " + data);
             $rootScope.$broadcast("quickNotification", "Something went wrong!" + data);
@@ -90,4 +95,22 @@ app.controller('homeController', function ($scope, $http, $rootScope) {
         $scope.loadPublicClubs();
         $scope.loadAllInterests();
     }
+  
+      //CUSTOM FILTER FOR INTERESTS
+    $scope.checkUserInterest = function(interest){
+            //interest == userInterests[i];
+            var temp = false;
+            if($scope.userInterests.length > 0){
+                for(i=0; i < $scope.userInterests.length; i++) {
+                    if(interest.interest == $scope.userInterests[i]){
+
+                        temp = true;
+                        break;
+                    }
+                }
+            }else{
+                temp = true;
+            }
+        return temp;        
+    };
 });
